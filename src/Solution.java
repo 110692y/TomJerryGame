dfada31717008946f2af835e4de74784a444bbe3
Yan <yan.skidkov@ctconsult.ru>
2018-03-27 15:53:48 +0300

    update

--- src/Solution.java
+++ src/Solution.java
@@ -1,62 +1,108 @@
 public class Solution {
     public static void main(String[] args) {
-        Dog spike = new Dog();
-        Cat tom = new Cat();
-        Mouse jerry = new Mouse();
-        spike.geoposition = 50;
-        tom.geoposition = 50;
-        jerry.geoposition = 100;
+        Dog spike = new Dog("Spike", 400);
+        Cat tom = new Cat("Tom", 300);
+        Mouse jerry = new Mouse("Jerry", 300);
 
-        tom.move(-6);
-        tom.move(6);
+        tom.move(100);
+        jerry.move(100);
         System.out.println("tom.geoposition: " + tom.geoposition);
         System.out.println("spike.geoposition: " + spike.geoposition);
+        System.out.println("jerry.geoposition: " + jerry.geoposition);
 
         if (tom.geoposition == jerry.geoposition) {
             jerry.status = false;
-            tom.hungry = true;
+            tom.hungry = false;
             System.out.println("Is Jerry alive? " + jerry.status + ". Is Tom hungry? " + tom.hungry);
         } else {
             jerry.status = true;
-            tom.hungry = false;
+            tom.hungry = true;
             System.out.println("Is Jerry alive? " + jerry.status + ". Is Tom hungry? " + tom.hungry);
         }
 
         if (spike.geoposition == tom.geoposition) {
             tom.status = false;
-            spike.hungry = true;
+            spike.hungry = false;
             System.out.println("Is Tom alive? " + tom.status + ". Is Spike hungry? " + spike.hungry);
         } else {
             tom.status = true;
-            spike.hungry = false;
+            spike.hungry = true;
             System.out.println("Is Tom alive? " + tom.status + ". Is Spike hungry? " + spike.hungry);
         }
     }
 
-    public interface Movable        {        void move(int distance);    }
-    public interface Eatable        {        boolean eaten();    }
-    public interface Eat            {        boolean eat();    }
+    public interface Movable {
+        void move(int distance);
+    }
+
+    public interface Eatable {
+        boolean eaten();
+    }
+
+    public interface Eat {
+        boolean eat();
+    }
 
     public static class Cat implements Eat, Eatable, Movable {
+        String name;
         int geoposition;
+
+        public Cat(String name, int geoposition) {
+            this.name = name;
+            this.geoposition = geoposition;
+        }
+
         boolean status;
         boolean hungry;
-        public boolean eat() {   return hungry;     }
-        public boolean eaten() {            return status;        }
-        public void move(int distance) {            geoposition += distance;        }
+
+        public boolean eat() {
+            return hungry;
+        }
+
+        public boolean eaten() {
+            return status;
+        }
+
+        public void move(int distance) {
+            geoposition += distance;
+        }
     }
 
     public static class Mouse implements Eatable, Movable {
+        String name;
         int geoposition;
+
+        public Mouse(String name, int geoposition) {
+            this.name = name;
+            this.geoposition = geoposition;
+        }
+
         boolean status;
-        public boolean eaten() {            return status;        }
-        public void move(int distance) {        }
+
+        public boolean eaten() {
+            return status;
+        }
+
+        public void move(int distance) {
+        }
     }
 
     public static class Dog implements Eat, Movable {
+        String name;
         int geoposition;
+
+        public Dog(String name, int geoposition) {
+            this.name = name;
+            this.geoposition = geoposition;
+        }
+
         boolean hungry;
-        public boolean eat() {   return hungry;     }
-        public void move(int distance) {        }
+
+        public boolean eat() {
+            return hungry;
+        }
+
+        public void move(int distance) {
+        }
     }
 }
